df93bd3075ddc9b8d8c12d84582f3a5cb39764e3
unknown <cromer.dalton@gmail.com>
2020-07-19 19:35:02 -0400

    added private route and changed the username/password for ease

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute exact path='/colors' component={BubblePage} />
       </div>
     </Router>
   );
