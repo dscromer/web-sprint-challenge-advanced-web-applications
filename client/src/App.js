a335fefd310f81d0b9af2089e686fd25f8647c72
unknown <cromer.dalton@gmail.com>
2020-07-19 21:40:16 -0400

    MVP met

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import PrivateRoute from './components/PrivateRoute'
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from './components/BubblePage'
 
 function App() {
   return (
